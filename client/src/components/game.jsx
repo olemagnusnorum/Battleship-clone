2f0c9441b51f3e569baebab55853e68cc60b838a
Ole-Magnus Vian Norum <ole99@live.no>
2022-08-17 17:45:47 +0200

    Fix player board on client side

--- client/src/components/game.jsx
+++ client/src/components/game.jsx
@@ -5,12 +5,16 @@ import '../css/tile.css'
 import '../css/playerBoard.css'
 import '../css/boatContainer.css'
 
+let prev = {"x": null, "y": null};
+
 function Game(props){
     
     const [board, setBoard] = useState(); 
     const [shipsOffsets, setShipsOffsets] = useState([]);
     const [shipsPlaced, setShipPlaced] = useState(new Set());
     const [currentShip, setCurrentShip] = useState();
+    const [shadowBoard, setShadowBoard] = useState();
+    const boardRef = useRef(null);
     
     const rotateShip = () =>{
         console.log("sendt rotate");
@@ -33,7 +37,31 @@ function Game(props){
 
     useEffect(() => {
         props.socket.emit('initialize_game');
+        // setup shadowBoard
     }, []);
+
+    useEffect(() => {
+        console.log(boardRef)
+        console.log(board)
+        const eventListener = (event) => {
+            const rect = boardRef.current?.getBoundingClientRect();
+            const tile = event.target
+            const x = +tile.dataset.x
+            const y = +tile.dataset.y
+            if (prev.x !== x || prev.y !== y){
+                // update shadowArray
+                prev.x = x;
+                prev.y = y;
+                console.log(x,y);
+            }
+        }
+
+        boardRef.current?.addEventListener('mousemove', eventListener)
+
+        return () => {
+            boardRef.current?.removeEventListener('mousemove', eventListener);
+        };
+    }, [boardRef.current])
     
     useEffect(() => {
         props.socket.on('initial_game_state', (data) => {
@@ -73,50 +101,84 @@ function Game(props){
     }, [props.socket, shipsOffsets, board]);
     
 
-    if (board !== undefined) {
-        return(
-            <div>
-                <p>THIS IS THE GAME: {props.roomNumber}</p>
-                <button onClick={rotateShip}>ROTATE 1</button>
-                <button onClick={placeShip}>place ship</button>
-                <div className="playerBoard">
-                    {board.map((row, rowId) => {
-                        return(
-                            <div key={rowId}>
-                            {row.map((value, columnId) => {
-                                    {switch(value){
-                                        case 0: 
-                                            return <div className="tileWater" key={columnId}></div>
-                                            case 1:
-                                                return <div className="tileShip" key={columnId}></div>
-                                            case 3:    
-                                                return <div className="tileMiss" key={columnId}></div>
-                                            case 4:    
-                                                return <div className="tileHit" key={columnId}></div>
-                                            case 5:    
-                                                return <div className="tileSunk" key={columnId}></div>
-                                    }}
-                                    <div className="tile"></div>
-                                })}
-                            </div>
-                        )
-                    })}
+
+    return(
+        <div>
+            <p>THIS IS THE GAME: {props.roomNumber}</p>
+            <button onClick={rotateShip}>ROTATE 1</button>
+            <button onClick={placeShip}>place ship</button>
+            <div className="board">
+
+            <div className="playerBoard" ref={boardRef}>
+                {board?.map((row, rowId) => {
+                    return(
+                        <div key={rowId}>
+                        {row.map((value, columnId) => {
+                            {switch(value){
+                                case 0: 
+                                    return <div className="tileWater" key={columnId} data-x={columnId} data-y={rowId}></div>
+                                case 1:
+                                    return <div className="tileShip" key={columnId} data-x={columnId} data-y={rowId}></div>
+                                case 3:    
+                                    return <div className="tileMiss" key={columnId} data-x={columnId} data-y={rowId}></div>
+                                case 4:    
+                                    return <div className="tileHit" key={columnId} data-x={columnId} data-y={rowId}></div>
+                                case 5:    
+                                    return <div className="tileSunk" key={columnId} data-x={columnId} data-y={rowId}></div>
+                                }}
+                            })}
+                        </div>
+                    )
+                })}
+            </div>
+            <div className="shadowBoard">
+                <div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
                 </div>
-                <div className="boatContainer">
-                    {shipsOffsets.map((shipOffset, shipOffsetId) => {
-                        return(
-                            <button onClick={() => handleSetCurrentShip(shipOffsetId)}>{shipOffsetId}</button>
-                        )
-                    })}
+                <div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                </div>
+                <div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
+                    <div className="tileShadow"></div>
                 </div>
             </div>
-        );
-    } else {
-        return(
-            <p>LOADING</p>
-        );
-    }
-    
+            </div>
+            <div className="boatContainer">
+                {shipsOffsets.map((shipOffset, shipOffsetId) => {
+                    return(
+                        <button onClick={() => handleSetCurrentShip(shipOffsetId)}>{shipOffsetId}</button>
+                    )
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default Game
